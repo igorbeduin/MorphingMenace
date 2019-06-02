b92bbcb61a7f517b5cef788f4fb2055d98173078
Unknown <raphaelrcbarbosa@gmail.com>
2019-06-01 22:10:39 -0300

    StageState as initial state

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,12 +5,10 @@ int main(int argc, char *argv[]) {
 
   Game& game = Game::GetInstance();//cria o game
 
-  // StageState* stageState = new StageState();
-  // game.Push(stageState);
-  TitleState* Start = new TitleState();
-  game.Push(Start);
-  // game.storedState = stageState;
-  // game.Push(storedState);
+  StageState* stageState = new StageState();
+  game.Push(stageState);
+  // TitleState* Start = new TitleState();
+  // game.Push(Start);
   game.Run();//executa o game até a janela ser fechada
   // game.~Game();//desaloca a memória, destrói as janelas, etc
   return 0;
