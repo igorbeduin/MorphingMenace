f6072ad174e7cf360c33e4089bd874af3622da7e
Igor Beduin <igor.rob.96@gmail.com>
2019-06-03 17:08:03 -0300

    Adding InputManager to StageState

--- src/StageState.cpp
+++ src/StageState.cpp
@@ -10,10 +10,24 @@ void StageState::LoadAssets()
 
 void StageState::Update(float dt)
 {
+    if (InputManager::GetInstance().KeyPress(ESCAPE_KEY) || InputManager::GetInstance().QuitRequested())
+    {
+        quitRequested = true;
+    }
+
+    // std::cout << "(int)objectArray.size(): " << (int)objectArray.size() << std::endl;
+    for (int i = 0; i < (int)objectArray.size(); i++)
+    {
+        objectArray[i]->Update(dt);
+    }
 }
 
 void StageState::Render()
 {
+    for (int i = 0; i < (int)objectArray.size(); i++)
+    {
+        objectArray[i]->Render();
+    }
 }
 
 StageState::~StageState()
