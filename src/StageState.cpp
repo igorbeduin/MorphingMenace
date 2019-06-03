642a0a5a3ce4d17269efe9ce65f8fc5d5cf9703a
Unknown <raphaelrcbarbosa@gmail.com>
2019-06-03 19:34:42 -0300

    TileMap object created

--- src/StageState.cpp
+++ src/StageState.cpp
@@ -24,6 +24,16 @@ StageState::StageState()
     planet->AddComponent(planet_sprite);
     planet_sprite->SetScale(PLANET_BACKGROUND_SCALE, PLANET_BACKGROUND_SCALE);
 
+    //Creating TileSet and TileMap
+    GameObject *tile_object = new GameObject();
+    std::weak_ptr<GameObject> weak_tile = AddObject(tile_object);
+    std::shared_ptr<GameObject> tileObject = weak_tile.lock();
+    tileObject->box.x = 0;
+    tileObject->box.y = 0;
+    TileSet* tileSet(new TileSet(*tileObject, TILE_WIDTH, TILE_HEIGHT, TILESET_PATH, {TILE_SCALE, TILE_SCALE} ));
+    std::shared_ptr<TileMap> tileMap(new TileMap(*tileObject, TILEMAP_PATH , tileSet));
+    tileObject->AddComponent(tileMap);
+
     //Creating player
     GameObject* player = new GameObject();
     std::shared_ptr<Character> playerBehaviour(new Character(*player, 1, char_type::PLAYER));
