7400d27c33374ee0ebe93ed09385fff0b7014c2b
Igor Beduin <igor.rob.96@gmail.com>
2019-06-03 23:06:43 -0300

    Forces mchanism implemented

--- include/Environment.h
+++ include/Environment.h
@@ -1,18 +1,23 @@
 #pragma once
 
-#include <vector>
+#define ENVIRONMENT
+#include "MorphingMenace_include.h"
 
 #include "Force.h"
-#include "GameObject.h"
+#include "Character.h"
+
+#include <vector>
+#include <memory>
 
 class Environment
 {
 public:
     Environment();
     ~Environment();
-    static void ApplyForces(GameObject *go);
+    static void AddForce(std::shared_ptr<Force> force);
+    static void ApplyForces(Character* character);
 
 private:
-    static std::vector<Force> forcesArray;
-    GameObject* go;
+    static std::vector<std::shared_ptr<Force>> forcesArray;
+    Character* character;
 };
