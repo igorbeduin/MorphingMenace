4a16ffc6bb06617a08ac681c8e66cad7828f1bfb
Igor Beduin <igor.rob.96@gmail.com>
2019-06-05 13:11:22 -0300

    Creating Enemy and Boss

--- src/Character.cpp
+++ src/Character.cpp
@@ -21,6 +21,21 @@ Character::Character(GameObject &associated, float mass, char_type charType) : C
     }
     case ENEMY:
     {
+        // PROVISORIO:
+        std::shared_ptr<Sprite> charSprite(new Sprite(associated, ENEMY_1_SPRITE_PATH, ENEMY_1_SPRITES_NUMB, ENEMY_1_SPRITES_TIME));
+        charSprite->SetScale(ENEMY_1_SCALE, ENEMY_1_SCALE);
+        associated.AddComponent(charSprite);
+
+        associated.moving = false;
+        // TODO:: Cria um personagem do tipo enemy
+        break;
+    }
+    case BOSS:
+    {
+        // PROVISORIO:
+        std::shared_ptr<Sprite> charSprite(new Sprite(associated, BOSS_SPRITE_PATH, BOSS_SPRITES_NUMB, BOSS_SPRITES_TIME));
+        charSprite->SetScale(BOSS_SCALE, BOSS_SCALE);
+        associated.AddComponent(charSprite);
         // TODO:: Cria um personagem do tipo enemy
         break;
     }
@@ -84,16 +99,21 @@ bool Character::Is(std::string type)
 {
     switch (charType)
     {
-    case PLAYER:
-    {
-        return (type == "Player" || type == "Character");
-        break;
-    }
-    case ENEMY:
-    {
-        return (type == "Enemy" || type == "Character");
-        break;
-    }
+        case PLAYER:
+        {
+            return (type == "Player" || type == "Character");
+            break;
+        }
+        case ENEMY:
+        {
+            return (type == "Enemy" || type == "Character");
+            break;
+        }
+        case BOSS:
+        {
+            return (type == "Boss" || type == "Character");
+            break;
+        }
     }
 }
 
