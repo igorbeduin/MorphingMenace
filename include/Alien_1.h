fdae32dc3ee89fda79357ee821c630e88f1d5f7f
Igor Beduin <igor.rob.96@gmail.com>
2019-07-01 02:33:19 -0300

    Alien 1 transformation created

---
+++ include/Alien_1.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#define CHARACTER
+#include "MorphingMenace_include.h"
+#include "Engine_include.h"
+#include "GameObject.h"
+#include "Component.h"
+#include "Transformation.h"
+#include "Sprite.h"
+
+class Sprite;
+
+class Alien_1 : public Component, public Transformation
+{
+public:
+    Alien_1(GameObject &associated);
+    void Update(float dt);
+    void Render();
+    bool Is(std::string type);
+    void Attack();
+    void Absorb();
+    void VerifyState();
+
+private:
+    std::shared_ptr<Sprite> sprite;
+};
