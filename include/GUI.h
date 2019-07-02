b4fe7988f6b5e59606079468ba707450494a2c0b
Igor Beduin <igor.rob.96@gmail.com>
2019-07-02 18:35:06 -0300

    Updating GUI logic

--- include/GUI.h
+++ include/GUI.h
@@ -1,12 +1,19 @@
 #pragma once
 
+#include "macros/GUI_include.h"
 #include "Component.h"
 #include "GameObject.h"
+#include "Sprite.h"
 
 class GUI : public Component
 {
 public:
+    GUI(GameObject& associated);
     void Update(float dt);
     void Render();
     bool Is(std::string type);
+private:
+    std::shared_ptr<Sprite> lifeSprite;
+    std::shared_ptr<Sprite> influenceSprite;
+
 };
--- src/GUI.cpp
+++ src/GUI.cpp
@@ -1,10 +1,27 @@
 #include "../include/GUI.h"
 
+GUI::GUI(GameObject &associated) : Component::Component(associated),
+                                   lifeSprite(new Sprite(associated, GUI_LIFE_PATH, GUI_LIFE_FRAMES_NUMBER, GUI_LIFE_FRAME_TIME)),
+                                   influenceSprite(new Sprite(associated, GUI_INFLUENCE_PATH, GUI_INFLUENCE_FRAMES_NUMBER, GUI_INFLUENCE_FRAME_TIME))
+{
+    lifeSprite->SetScale(GUI_SCALE, GUI_SCALE);
+    influenceSprite->SetScale(GUI_SCALE, GUI_SCALE);
+}
+
 void GUI::Update(float dt)
-{}
+{
+    lifeSprite->Update(dt);
+    influenceSprite->Update(dt);
+    associated.box.x += 15;
+    associated.box.y += -30;
+
+}
 
 void GUI::Render()
-{}
+{
+    lifeSprite->Render();
+    influenceSprite->Render();
+}
 
 bool GUI::Is(std::string type)
 {
