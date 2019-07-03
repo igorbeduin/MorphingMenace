ad7eba6c1f71aa66179af7a7c6ddce5c6eb976fd
Unknown <raphaelrcbarbosa@gmail.com>
2019-07-03 03:56:47 -0300

    Changing focus limit for new window size

--- include/Engine_include.h
+++ include/Engine_include.h
@@ -17,9 +17,9 @@
 #define CAMERA_SPEED 100
 #define MAX_CAM_SPEED_X 300
 #define MAX_CAM_SPEED_Y 300
-#define LEFT_FOCUS_LIMIT 342
-#define RIGHT_FOCUS_LIMIT 482
-#define UP_FOCUS_LIMIT 300
+#define LEFT_FOCUS_LIMIT 450
+#define RIGHT_FOCUS_LIMIT 800
+#define UP_FOCUS_LIMIT 200
 #define DOWN_FOCUS_LIMIT 500
 
 /************************************************
