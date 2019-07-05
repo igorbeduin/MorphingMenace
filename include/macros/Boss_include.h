4d16d7f8ee071b6bb314aa1c44b962d4ff8d71d0
Unknown <raphaelrcbarbosa@gmail.com>
2019-07-05 16:14:59 -0300

    Changes in boss

--- include/macros/Boss_include.h
+++ include/macros/Boss_include.h
@@ -1,6 +1,6 @@
-#define BOSS_SPRITE_PATH "assets/img/char/boss_temporario.png"
+#define BOSS_SPRITE_PATH "assets/img/char/hivemind.png"
 #define BOSS_SPRITES_NUMB 1
 #define BOSS_SPRITES_TIME 1
-#define BOSS_SCALE 0.20
-#define BOSS_INIT_POS {4400, 3300}
+#define BOSS_SCALE 1.1
+#define BOSS_INIT_POS {3858, 2477}
 //#define BOSS_INIT_POS {0, 0}
