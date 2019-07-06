e1ce24415a8f5307cdaa5531ddcb1030be78bec1
Unknown <raphaelrcbarbosa@gmail.com>
2019-07-06 03:04:27 -0300

    Start and coreArray created

--- include/Boss.h
+++ include/Boss.h
@@ -9,10 +9,14 @@
 class Boss : public Component
 {
 public:
+
     Boss(GameObject &associated);
     void Update(float dt);
     void Render();
     bool Is(std::string type);
+    void Start();
+    std::vector< std::weak_ptr<GameObject> > coreArray;
+
 private:
     int influenceReference;
 };
