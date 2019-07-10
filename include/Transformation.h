5275a7b114434b391b3e4b5cbc251380c242ee68
Igor Beduin <igor.rob.96@gmail.com>
2019-07-10 01:44:10 -0300

    Transformation with Component's virtuals methods

--- include/Transformation.h
+++ include/Transformation.h
@@ -6,6 +6,10 @@
 #include "GameObject.h"
 #include "Collider.h"
 #include <iostream>
+#include "Sprite.h"
+
+class Sprite;
+
 
 class Transformation
 {
@@ -13,5 +17,10 @@ public:
     virtual void Attack() = 0;
     virtual void Absorb();
     virtual void Update(float dt) = 0;
+    virtual void Render() = 0;
     void Downgrade();
+    virtual float GetWidth();
+    virtual float GetHeight();
+protected:
+    std::shared_ptr<Sprite> sprite;
 };
