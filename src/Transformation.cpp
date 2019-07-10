5e95a31f0ed57aff8a5388cf16208d8b042c8431
Igor Beduin <igor.rob.96@gmail.com>
2019-07-10 01:41:56 -0300

    Transformation Getters for sprite Height and Width

--- src/Transformation.cpp
+++ src/Transformation.cpp
@@ -8,3 +8,12 @@ void Transformation::Absorb()
     Player::player->LvlDown();
     Player::player->Absorb();
 }
+
+float Transformation::GetWidth()
+{
+    return sprite->GetWidth();
+}
+float Transformation::GetHeight()
+{
+    return sprite->GetHeight();
+}
