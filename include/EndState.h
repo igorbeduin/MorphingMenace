f49a6f8e738243314d3340d5e0745153235c3e9a
Unknown <raphaelrcbarbosa@gmail.com>
2019-07-11 16:56:31 -0300

    EndState lose conditions and screen implemented

--- include/EndState.h
+++ include/EndState.h
@@ -1,7 +1,27 @@
 #pragma once
 
-#include "Music.h"
+
+#define STATE
+#include "MorphingMenace_include.h"
+#include "Engine_include.h"
 #include "State.h"
+#include "Sprite.h"
+#include "StageState.h"
+#include "TitleState.h"
+#include "Text.h"
+#include "Music.h"
+
+// #define VICTORY_MSG "PRESS SPACEBAR TO PLAY AGAIN"
+// #define DEFEAT_MSG "PRESS SPACEBAR FOR REVENGE"
+// #define VICTORY_MUSIC "assets/audio/endStateWin.ogg"
+// #define DEFEAT_MUSIC "assets/audio/endStateLose.ogg"
+
+// #define DEFEAT_COLOR {255, 0, 30, 255}
+// #define INSTRUCTION_MSG "PRESS ESC TO LEAVE THE GAME"
+
+// #define FONT_PATH "assets/font/Call me maybe.ttf"
+// #define END_FONT_SIZE 40
+// #define END_SCREEN_TIME 1
 
 class EndState : public State {
 
