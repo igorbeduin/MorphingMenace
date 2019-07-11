c33bec35c0e1b2493be7f27e1ce88d7217a4c0d2
Unknown <raphaelrcbarbosa@gmail.com>
2019-07-11 15:36:10 -0300

    Game starts in titleState

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,12 +5,11 @@ int main(int argc, char *argv[]) {
 
   Game& game = Game::GetInstance();//cria o game
 
-  StageState* stageState = new StageState();
-  game.Push(stageState);
-  // TitleState* Start = new TitleState();
-  // game.Push(Start);
+  // StageState* stageState = new StageState();
+  // game.Push(stageState);
+  TitleState* Start = new TitleState();
+  game.Push(Start);
   game.Run();//executa o game até a janela ser fechada
-  // game.~Game();//desaloca a memória, destrói as janelas, etc
   return 0;
 
 }
