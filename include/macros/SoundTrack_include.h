76cad9dd9e0e17cf7b361387e0f1c74b4be150e0
Unknown <raphaelrcbarbosa@gmail.com>
2019-07-11 21:10:47 -0300

    SoundTrack component implemented

---
+++ include/macros/SoundTrack_include.h
@@ -0,0 +1,28 @@
+#define LVL0_OPEN_AREA_PATH "assets/audio/Tema A forma bebê testar loop.ogg"
+#define LVL1_OPEN_AREA_PATH "assets/audio/Tema A2 - Forma adulta 1° inimigo teste loop.ogg"
+#define LVL0_WATER1_PATH "assets/audio/background_demo1.ogg"
+
+#define OPEN_AREA_X 0
+#define OPEN_AREA_Y 0
+#define OPEN_AREA_W 6326
+#define OPEN_AREA_H 1303
+
+#define BOSS_LAIR_X 3835
+#define BOSS_LAIR_Y 2685
+#define BOSS_LAIR_W 1275
+#define BOSS_LAIR_H 655
+
+#define WATER1_X 1370
+#define WATER1_Y 1300
+#define WATER1_W 2455
+#define WATER1_H 1409
+
+#define WATER2_X 3900
+#define WATER2_Y 1332
+#define WATER2_W 2420
+#define WATER2_H 698
+
+#define UNDERDARK_X 
+#define UNDERDARK_Y 
+#define UNDERDARK_W 
+#define UNDERDARK_H 
